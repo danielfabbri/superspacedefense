428f386a1b964912a41add6fc906ed3d4d6b4829
Daniel Fabbri <daniel.fabbri@superuber.com>
2015-06-25 14:10:02 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready( function enemyArrive() {
+$("#e1").animate({'margin-right': '-130px'}, 1);
+$("#e1").animate({'margin-right': '20px'}, 19000 ?enemyMove:function(){});
+function enemyMove() {
+var random = Math.random();
+    if (random <= 0.3) {
+        $("#e1").animate({'margin-top': '20px'}, 2000 ?enemyMove:function(){});
+    } else if (random <= 0.6) {
+        $("#e1").animate({'margin-top': '270px'}, 2000 ?enemyMove:function(){});
+    } else if (random <= 1) {
+        $("#e1").animate({'margin-top': '540px'}, 2000 ?enemyMove:function(){});
+    }
+}
+});
+
+$(document).keydown(function(e) {
+var pPosition = $("#p1").css('margin-top');
+var ePosition = $("#e1").css('margin-top');
+
+    switch(e.which) {
+        case 37: // left
+        prompt('Deseja sair? (YES) (NO)');
+        break;
+
+        case 38: // up
+        $("#p1").animate({'margin-top': '-=270px'});
+        break;
+
+        case 39: // right
+        $("#shot").animate({'margin-left': '1020px'});
+        $("#shot").animate({'margin-left': '11px'}, 1);
+        var ballTop = $("#shot").offset().top;
+        var enemyTop = $("#e1").offset().top;
+        var ballSide = $("#shot").offset().left;
+        var enemySide = $("#e1").offset().left;
+            if (ballTop > enemyTop && ballTop < enemyTop + 90) {
+                setTimeout( function() {
+                    $("#e1").css({'background-image': 'url(\'images/nave3.png\')'})
+                    .animate({'margin-right': '-200px'}, 2000);
+                });
+            }
+        break;
+
+        case 40: // down
+        $("#p1").animate({'margin-top': '+=270px'});
+        
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
